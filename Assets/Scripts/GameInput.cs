2b28a554dac973c80594e3dcb36b274fe58c7cba
alfavit-101 <tetris00009@gmail.com>
2024-10-01 22:05:19 +0100

    Sprite Animation
    Добавил адекватный спрайт с анимацией вместо свиньи

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
 
-    public static GameInput instance { get; private set; }
+    public static GameInput Instance { get; private set; }
 
     private PlayerInputActions playerInputActions;
 
     private void Awake()
     {
-        instance = this;
+        Instance = this;
 
         playerInputActions = new PlayerInputActions();
         playerInputActions.Enable();
@@ -21,6 +22,14 @@ public class GameInput : MonoBehaviour
     {
         Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
         return inputVector;
+    }
+
+
+    //определяем положение курсора для поворота спрайта
+    public Vector3 GetMousePosition() 
+    {
+        Vector3 mousePos = Mouse.current.position.ReadValue();
+        return mousePos;
     }
 
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,22 +4,57 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public static Player Instance { get; private set; }
+
+
     [SerializeField] private float movementSpeed = 10f;
 
     private PlayerInputActions playerInputActions;
 
     private Rigidbody2D rb;
 
+    private float minMovementSpeed = 0.1f;
+    private bool isRunning = false;
+
     private void Awake()
     {
+        Instance = this;   
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
-        Vector2 inputVector = GameInput.instance.GetMovementVector();
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 inputVector = GameInput.Instance.GetMovementVector();
         inputVector = inputVector.normalized;
         rb.MovePosition(rb.position + (inputVector * movementSpeed * Time.fixedDeltaTime));
+
+
+       //проверяем бежит герой или нет
+        if (Mathf.Abs(inputVector.x) > minMovementSpeed || Mathf.Abs(inputVector.y) > minMovementSpeed) 
+        {
+            isRunning = true;
+        }
+        else 
+        {
+            isRunning = false;
+        }
+    }
+    
+    public bool IsRunning() 
+    {
+        return isRunning;  
+    }
+
+    //возвращает положение героя на экране
+    public Vector3 GetPlayerScreenPosition() 
+    {
+        Vector3 playerScreenPosition = Camera.main.WorldToScreenPoint(transform.position);
+        return playerScreenPosition;
     }
 
 }
