64c1f4b9386ecf6c4bc804caa2208b9be3450b95
alfavit-101 <tetris00009@gmail.com>
2024-10-02 15:43:54 +0100

    новая модель, коллизии

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -30,7 +30,6 @@ public class Player : MonoBehaviour
     private void HandleMovement()
     {
         Vector2 inputVector = GameInput.Instance.GetMovementVector();
-        inputVector = inputVector.normalized;
         rb.MovePosition(rb.position + (inputVector * movementSpeed * Time.fixedDeltaTime));
 
 
