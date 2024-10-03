c11c563a46c7c804d43be6054962cd35d5973848
alfavit-101 <tetris00009@gmail.com>
2024-10-03 16:36:27 +0100

    поворот посоха

--- Assets/Scripts/Weapons/ActiveWeapon.cs
+++ Assets/Scripts/Weapons/ActiveWeapon.cs
@@ -13,8 +13,29 @@ public class ActiveWeapon : MonoBehaviour
         Instance = this;
     }
 
-    public Wand GetActiveWeapon() 
+    private void Update()
+    {
+        FollowMousePosition(); //каждый кадр проверяет следует ли оружие за курсором
+    }
+
+    public Wand GetActiveWeapon()
     {
         return wand;
+    }
+
+    //поворачивает оружие в сторону курсора
+    private void FollowMousePosition()
+    {
+        Vector3 mousePos = GameInput.Instance.GetMousePosition(); //смотрит координаты курсора
+        Vector3 playerPosition = Player.Instance.GetPlayerScreenPosition(); //смотрит координаты героя
+
+        if (mousePos.x < playerPosition.x) //если координата курсора по оси х меньше оси х героя, то разворачивает оружие
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 }
